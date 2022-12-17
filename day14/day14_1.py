a885953ef660778a2b036d5a6e04223c59313559
CatSack <bossness544@gmail.com>
2022-12-16 20:04:08 -0800

    day 14 part 1

--- day14/day14_1.py
+++ day14/day14_1.py
@@ -5,10 +5,10 @@ sand = set()
 
 def fill_line(a, b):
     if a[0] == b[0]:
-        for i in range(min(a[1], b[1] + 1), max(a[1], b[1] + 1)):
+        for i in range(min(a[1], b[1]), max(a[1], b[1]) + 1):
             rock.add((a[0], i))
     elif a[1] == b[1]:
-        for i in range(min(a[0] + 1, b[0]), max(a[0] + 1, b[0])):
+        for i in range(min(a[0], b[0]), max(a[0], b[0]) + 1):
             rock.add((i, a[1]))
 
 for line in f.readlines():
@@ -24,7 +24,6 @@ def add_coords(a, b):
     return (a[0]+b[0], a[1]+b[1])
 
 def iterate(pos):
-    # print(pos)
     combined = rock.union(sand)
     filtered = list(filter(lambda p: p[0] == pos[0] and p[1] >= pos[1], combined))
     if len(filtered) == 0:
@@ -32,9 +31,8 @@ def iterate(pos):
     else:
         nearest_floor = sorted(filtered, key=lambda x: x[1])[0]
         nearest_floor = (nearest_floor[0], nearest_floor[1] - 1)
-    offsets = [(0, 1), (-1, 1), (1, 1)]
+    offsets = [(-1, 1), (1, 1)]
     if nearest_floor != pos:
-        # print("jumped")
         return nearest_floor
     for offset in offsets:
         new = add_coords(pos, offset)
@@ -43,7 +41,6 @@ def iterate(pos):
     return -2
 
 def sim_sand():
-    # while True:
     global sand
     finished = False
     while not finished:
@@ -70,12 +67,11 @@ def print_field():
             if (j, i) in rock:
                 print("#", end="")
             elif (j, i) in sand:
-                print("o", end="")
+                print("\033[96mo\033[0m", end="")
             else:
                 print(".", end="")
         print("")
 
-# print(rock)
 sim_sand()
 print_field()
 print(len(sand))
